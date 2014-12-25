606034e88b86ef4896f1b97db575dccd8297e571
Loki26 <sergey.loki26@gmail.com>
2014-12-25 16:00:42 +0200

    test app 1st upload

---
+++ gen/com/block_shema/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.block_shema;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/block_shema/MainActivity.java
@@ -0,0 +1,64 @@
+package com.block_shema;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity implements OnClickListener
+{
+
+	EditText edt1, edt2;
+	Button btn;
+	TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        edt1 = (EditText) findViewById(R.id.editText1);
+        edt2 = (EditText) findViewById(R.id.editText2);
+        
+        btn = (Button) findViewById(R.id.button1);
+        txt = (TextView) findViewById(R.id.textView1);
+        
+        btn.setOnClickListener(this);
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		float a = Float.parseFloat(edt1.getText().toString());
+		float b = Float.parseFloat(edt2.getText().toString());
+		float sum = a + b;
+		txt.setText(Float.toString(sum));
+	}
+
+}
